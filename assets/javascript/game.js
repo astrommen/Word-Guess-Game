51cd2d9afdd16f1a39bbe8fe55dbefa86c3a26cb
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-01-03 22:02:19 -0500

    test

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,5 @@
+var games = ["Super Mario World", "Final Fantasy III", "Sonic the Hedgehog", "Road Rash", "Legend of Zelda", "Mortal Kombat", "Wolfstein", "Dune", "Doom", "Virtua Fighter", "Master of Orion", "Warcraft", "Star Wars Dark Forces", "Need for Speed", "Quake"];
 
+document.getElementById("demo").innerHTML = games[0];
+
+document.write(games.length);
