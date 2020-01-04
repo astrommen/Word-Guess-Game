684d3dc351ede0bf6fd7789e8d1ee1e1e8b63f63
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-01-03 22:18:51 -0500

    + code to create new gamesBlank array by function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,10 @@
 var games = ["Super Mario World", "Final Fantasy III", "Sonic the Hedgehog", "Road Rash", "Legend of Zelda", "Mortal Kombat", "Wolfstein", "Dune", "Doom", "Virtua Fighter", "Master of Orion", "Warcraft", "Star Wars Dark Forces", "Need for Speed", "Quake"];
 
-document.getElementById("demo").innerHTML = games[0];
+var gamesBlank = games.map(myFunction); //This line creates a new array by performing a function on each array element
 
-document.write(games.length);
+document.getElementById("demo").innerHTML = games[0]; //This line prints an element from the array
+
+document.write(games.length); //This line prints the length of the array
+
+// The code below will join all array elements into a string, using * as a separator
+// document.getElementById("demo").innerHTML = games.join(" * "); 
