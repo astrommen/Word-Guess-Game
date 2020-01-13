8ef96be4fe3c9c6bb716665fb57d206fa19de166
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-01-13 08:53:18 -0500

    finally got correct user keys to replace blanks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,8 @@ var games = ["Super Mario World", "Final Fantasy III", "Sonic the Hedgehog",
 
 //empty array for comparison to user keys            
 var arr = [];
+var arrblanks = [];
+var userText2;
 
 // Creating variables to hold # of guesses & wins
 var guessesLeft = 12;
@@ -14,7 +16,7 @@ var wins = 0;
 
 // Create variables that hold refs to places in HTML where we want to display
 var directionsText = document.getElementById("directions-text");
-var userText = document.getElementById("user-text");
+var userText2 = document.getElementById("user-text");
 var winsText = document.getElementById("wins-text");
 var guessesText = document.getElementById("guesses-text");
 var blanksText = document.getElementById("blanks")
@@ -25,31 +27,48 @@ var computerChoice = games[Math.floor(Math.random() * games.length)];
 
 console.log(computerChoice);//For testing purposes
 
+//For loop that writes "_ " for every character
+for (i = 0; i < computerChoice.length; i++) {
+    
+    //If/ElseIf to check for spaces and add them
+    //This works
+    if (computerChoice[i] !== " "){
+        
+        // blanksText.textContent += "_ ";
+        arrblanks.push("_");
+        
+    } else if (computerChoice[i] === " "){
+        
+        // blanksText.textContent += "\xa0\xa0";
+        arrblanks.push("\xa0\xa0");
+    }   
+}
+
+//Prints the blanks to html
+blanksText.textContent += arrblanks.join(" ");
+
 //This code will listen for user keyboard inputs
 document.onkeyup = function(event) {
     
     //Prints user key strokes
-    userText.textContent += event.key + " "; 
-    userText.textContent = event.key;
-}  
-//For loop that writes "_ " for every character
-for (var i = 0; i < computerChoice.length; i++) {
+    userText2 = event.key;//used for win/guesses leftcheck
     
-    //If/ElseIf to check for spaces and add them
-		//This works
-    if (computerChoice[i] !== " "){
-
-        blanksText.textContent += "_ ";
-
-    } else if (computerChoice[i] === " "){
-
-        blanksText.textContent += "\xa0\xa0";
+    for (j = 0; j < computerChoice.length; j++) {   
+        
+        if (userText2 == computerChoice[j]){ 
+            //Changes blanks in position to userText
+            arrblanks[j] = userText2;
+            //Prints new arrblanks to html
+			blanksText.textContent = arrblanks.join(" ");
+            
+        } // cant get the else to work else {
+          //  guessesLeft--;
+          //  userText2.textContent += event.key + " "; 
+            
+            
+          //}  
     }
-
-    //This line pushes each char into the arr array
-    arr.push(computerChoice[i]);
 }
-
 
 
 
